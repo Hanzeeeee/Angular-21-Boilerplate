1f0e74ec1564168b0cc462364416694eabe60e36
Hanzeee <kagornigaw@gmail.com>
2026-05-13 00:41:13 +0800

    Remove unused/duplicate files

--- README.md
+++
@@ -1,1 +0,0 @@
-Node.js, TypeScript & MySQL Boilerplate API - RALLOS 
--- src/app/admin/overview.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-admin-overview',
-  templateUrl: './overview.component.html', standalone: false
-})
-export class OverviewComponent {
-}
